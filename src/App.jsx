800ef8f9cdeab81b032171c4c29cffc50bb8210c
Mahim Golder <mahimgolder@gmail.com>
2026-02-08 22:50:02 +0600

    customize the board component

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,9 @@
-import './App.css';
+import './style.css';
 
 function Square(){
   return (
     <>
-      <div className="board">
-        <div className="board-row">
-          <button className="square">o</button>
-          <button className="square">x</button>
-          <button className="square">x</button>
-        </div>
-        <div className="board-row">
-          <button className="square">x</button>
-          <button className="square">x</button>
-          <button className="square">x</button>
-        </div>
-        <div className="board-row">
-          <button className="square">x</button>
-          <button className="square">x</button>
-          <button className="square">x</button>
-        </div>
-      </div>
-
+      
     </>
   );
 }
@@ -30,7 +13,21 @@ export default function Board(){
   
   return (
     <>
-      <Square></Square>
+      <div className='board-row'>
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className='board-row'>
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className='board-row'>
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
     </>
   );
 }
