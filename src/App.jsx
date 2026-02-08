f10cb8405af8ec0a1f140b85dcac39874419837a
Mahim Golder <mahimgolder@gmail.com>
2026-02-08 07:57:30 +0600

    Builded the board component Done

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+export default function Board(){
+  return (
+    <>
+      <div className="board">
+        <div className="board-row">
+        <button className="square">o</button>
+        <button className="square">x</button>
+        <button className="square">x</button>
+        </div>
+      <div className="board-row">
+        <button className="square">x</button>
+        <button className="square">x</button>
+        <button className="square">x</button>
+      </div>
+      <div className="board-row">
+        <button className="square">x</button>
+        <button className="square">x</button>
+        <button className="square">x</button>
+      </div>
+      </div>
+
+
+      
+    </>
+  );
+}
