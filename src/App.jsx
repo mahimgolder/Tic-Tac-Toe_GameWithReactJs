f1936dbd3091d3724ae82be5bfd0e1a9c1d18864
Mahim Golder <mahimgolder@gmail.com>
2026-02-08 08:40:08 +0600

    Updated board component to render the suquare component useing JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,36 @@
 import './App.css';
 
-export default function Board(){
+function Square(){
   return (
     <>
       <div className="board">
         <div className="board-row">
-        <button className="square">o</button>
-        <button className="square">x</button>
-        <button className="square">x</button>
+          <button className="square">o</button>
+          <button className="square">x</button>
+          <button className="square">x</button>
         </div>
-      <div className="board-row">
-        <button className="square">x</button>
-        <button className="square">x</button>
-        <button className="square">x</button>
-      </div>
-      <div className="board-row">
-        <button className="square">x</button>
-        <button className="square">x</button>
-        <button className="square">x</button>
-      </div>
+        <div className="board-row">
+          <button className="square">x</button>
+          <button className="square">x</button>
+          <button className="square">x</button>
+        </div>
+        <div className="board-row">
+          <button className="square">x</button>
+          <button className="square">x</button>
+          <button className="square">x</button>
+        </div>
       </div>
 
+    </>
+  );
+}
 
-      
+
+export default function Board(){
+  
+  return (
+    <>
+      <Square></Square>
     </>
   );
 }
