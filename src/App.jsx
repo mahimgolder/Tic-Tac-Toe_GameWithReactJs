cdfad4f226e83f6fc514df8d1b0b33ec9349cdd0
Mahim Golder <mahimgolder@gmail.com>
2026-02-25 02:21:33 +0600

    completed the completing section

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import './style.css';
 
 function Square({ value, onSquareClick }){
 
-  // Click Handelfunction here
-  function clikHandle(){
-    setValue("x");
-  }
 
 
   return (
@@ -18,24 +14,46 @@ function Square({ value, onSquareClick }){
 
 
 export default function Board(){
+  const [xIsNext, setXIsNext] = useState(true);
   const[squares, setSquares] = useState(Array(9).fill(null));
+
+
   
+
+
+  function clickHandel(i) {
+    const nextSquares = squares.slice();
+
+    if (xIsNext) {
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
+    }
+
+    
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+
+    
+  }
+
+
   return (
     <>
       <div className='board-row'>
-        <Square value={squares[0]}></Square>
-        <Square value={squares[1]}></Square>
-        <Square value={squares[2]}></Square>
+        <Square value={squares[0]} onSquareClick={() => clickHandel(0)}></Square>
+        <Square value={squares[1]} onSquareClick={() => clickHandel(1)}></Square>
+        <Square value={squares[2]} onSquareClick={() => clickHandel(2)}></Square>
       </div>
       <div className='board-row'>
-        <Square value={squares[3]}></Square>
-        <Square value={squares[4]}></Square>
-        <Square value={squares[5]}></Square>
+        <Square value={squares[3]} onSquareClick={() => clickHandel(3)}></Square>
+        <Square value={squares[4]} onSquareClick={() => clickHandel(4)}></Square>
+        <Square value={squares[5]} onSquareClick={() => clickHandel(5)}></Square>
       </div>
       <div className='board-row'>
-        <Square value={squares[6]}></Square>
-        <Square value={squares[7]}></Square>
-        <Square value={squares[8]}></Square>
+        <Square value={squares[6]} onSquareClick={() => clickHandel(6)}></Square>
+        <Square value={squares[7]} onSquareClick={() => clickHandel(7)}></Square>
+        <Square value={squares[8]} onSquareClick={() => clickHandel(8)}></Square>
       </div>
     </>
   );
